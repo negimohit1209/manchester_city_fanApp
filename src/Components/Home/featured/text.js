a141858bfdfcb331ead52881cd48cc04a90dc335
negimohit1209 <mohitsngh1997@gmail.com>
2019-10-23 23:59:43 +0530

    adds the player animation in the home page

--- src/Components/Home/featured/text.js
+++ src/Components/Home/featured/text.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { easePolyOut } from 'd3-ease';
 import Animate from 'react-move/Animate';
+import FeaturedPlayer from '../../../Resources/images/featured_player.png';
 
 export default class Text extends Component {
     animateNumber = () => (
@@ -21,7 +22,7 @@ export default class Text extends Component {
                         style={{
                             opacity,
                             transform: `translate(260px,170px) rotateY(${rotate}deg)`
-                        }}>3</div>
+                        }}>4</div>
                 )
             }}
         </Animate>
@@ -51,13 +52,64 @@ export default class Text extends Component {
                 )
             }}
         </Animate>
-    )
+    );
+    animateSecond = () => (
+        <Animate
+            show
+            start={{
+                opacity: 0,
+                x: 503,
+                y: 586
+            }}
+            enter={{
+                opacity: [1],
+                x: [273],
+                y: [586],
+                timing: { delay: 300, duration: 500, ease: easePolyOut }
+            }}
+        >
+            {({ opacity, x, y }) => {
+                return (
+                    <div className="featured_first"
+                        style={{
+                            opacity,
+                            transform: `translate(${x}px,${y}px)`
+                        }}>Championships</div>
+                )
+            }}
+        </Animate>
+    );
+    animatePlayer = () => (
+        <Animate
+            show
+            start={{
+                opacity: 0,
+            }}
+            enter={{
+                opacity: [1],
+                timing: { delay: 800, duration: 500, ease: easePolyOut }
+            }}
+        >
+            {({ opacity }) => {
+                return (
+                    <div className="featured_player"
+                        style={{
+                            opacity,
+                            background: `url(${FeaturedPlayer})`,
+                            transform: `translate(550px,201px)`
+                        }}></div>
+                )
+            }}
+        </Animate >
+    );
+
     render() {
         return (
             <div className="featured_text">
+                {this.animatePlayer()}
                 {this.animateNumber()}
                 {this.animateFirst()}
-
+                {this.animateSecond()}
             </div>
         )
     }
