21bf865d16e040dc002ab00fc8d616b72559c17e
negimohit1209 <mohitsngh1997@gmail.com>
2020-02-25 12:56:13 +0530

    add formfeilds to editmatch page

--- src/Components/UI/formFeilds.js
+++ src/Components/UI/formFeilds.js
@@ -29,6 +29,26 @@ export default function FormFeilds({ formData, id, change }) {
           </div>
         );
         break;
+      case 'select':
+        formTemplate = (
+          <div>
+            {formData.showlabel ? (
+              <div className="label_inputs">{formData.config.label}</div>
+            ) : null}
+            <select
+              value={formData.value}
+              onChange={event => change({ event, id })}>
+              <option value="">Select one</option>
+              {formData.config.options.map(item => (
+                <option key={item.key} value={item.key}>
+                  {item.value}
+                </option>
+              ))}
+            </select>
+            {showError()}
+          </div>
+        );
+        break;
       default:
         formTemplate = null;
     }
--- src/Components/admin/matches/addEditMatch.js
+++ src/Components/admin/matches/addEditMatch.js
@@ -42,6 +42,82 @@ export default class AddEditMatch extends Component {
         valid: false,
         validationMessage: '',
         showlabel: false
+      },
+      resultLocal: {
+        element: 'input',
+        value: '',
+        config: {
+          label: 'result Local',
+          name: 'result_local_input',
+          type: 'text'
+        },
+        validation: {
+          required: true
+        },
+        valid: false,
+        validationMessage: '',
+        showlabel: false
+      },
+      away: {
+        element: 'select',
+        value: '',
+        config: {
+          label: 'Select an away team',
+          name: 'select_away',
+          type: 'select',
+          options: []
+        },
+        validation: {
+          required: true
+        },
+        valid: false,
+        validationMessage: '',
+        showlabel: false
+      },
+      resultAway: {
+        element: 'input',
+        value: '',
+        config: {
+          label: 'result away',
+          name: 'result_away_input',
+          type: 'text'
+        },
+        validation: {
+          required: true
+        },
+        valid: false,
+        validationMessage: '',
+        showlabel: false
+      },
+      referee: {
+        element: 'input',
+        value: '',
+        config: {
+          label: 'Referee',
+          name: 'referee_input',
+          type: 'text'
+        },
+        validation: {
+          required: true
+        },
+        valid: false,
+        validationMessage: '',
+        showlabel: true
+      },
+      stadium: {
+        element: 'input',
+        value: '',
+        config: {
+          label: 'Stadium',
+          name: 'stadium_input',
+          type: 'text'
+        },
+        validation: {
+          required: true
+        },
+        valid: false,
+        validationMessage: '',
+        showlabel: true
       }
     }
   };
@@ -57,11 +133,56 @@ export default class AddEditMatch extends Component {
                 formData={this.state.formData.date}
                 change={element => this.updateForm(element)}
               />
-              <FormFeilds
-                id="local"
-                formData={this.state.formData.local}
-                change={element => this.updateForm(element)}
-              />
+              <div className="select_team_layout">
+                <div className="label_inputs">Local</div>
+                <div className="wrapper">
+                  <div className="left">
+                    <FormFeilds
+                      id="local"
+                      formData={this.state.formData.local}
+                      change={element => this.updateForm(element)}
+                    />
+                  </div>
+                  <div>
+                    <FormFeilds
+                      id="resultLocal"
+                      formData={this.state.formData.resultLocal}
+                      change={element => this.updateForm(element)}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="select_team_layout">
+                <div className="label_inputs">Away</div>
+                <div className="wrapper">
+                  <div className="left">
+                    <FormFeilds
+                      id="local"
+                      formData={this.state.formData.away}
+                      change={element => this.updateForm(element)}
+                    />
+                  </div>
+                  <div>
+                    <FormFeilds
+                      id="resultLocal"
+                      formData={this.state.formData.resultAway}
+                      change={element => this.updateForm(element)}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="split_fields">
+                <FormFeilds
+                  id="referee"
+                  formData={this.state.formData.referee}
+                  change={element => this.updateForm(element)}
+                />
+                <FormFeilds
+                  id="stadium"
+                  formData={this.state.formData.stadium}
+                  change={element => this.updateForm(element)}
+                />
+              </div>
             </form>
           </div>
         </div>
